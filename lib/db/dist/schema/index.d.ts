f9211d8c4c5c9b13e35da4e04eec5ffcbdece595
Ubbaale <166643398+Ubbaale@users.noreply.github.com>
2026-04-14 11:33:32 -0400

    Add lib/db/dist/ build output files

---
+++ lib/db/dist/schema/index.d.ts
@@ -0,0 +1,9 @@
+export * from "./homes";
+export * from "./staff";
+export * from "./patients";
+export * from "./medications";
+export * from "./medication-administrations";
+export * from "./incidents";
+export * from "./daily-logs";
+export * from "./shifts";
+//# sourceMappingURL=index.d.ts.map
